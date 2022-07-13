f293befd8c00b257bf587210484b07c59688a3f7
clarkalvarez <clarknikka012@gmail.com>
2022-07-14 07:22:16 +0800

    13. Implement the AppState in the Angular NGRX Application by combining all the Feature states.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component'; 
-import { StoreModule } from '@ngrx/store'; 
-import { counterReducer } from './counter/state/counter.reducer';
+import { StoreModule } from '@ngrx/store';  
 import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
@@ -14,7 +12,8 @@ import { CounterOutputComponent } from './counter/counter-output/counter-output.
 import { HeaderComponent } from './shared/components/header/header.component';
 import { PostsListComponent } from './posts/posts-list/posts-list.component'; 
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment'; 
+import { appReducer } from './store/app.state';
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,7 +29,7 @@ import { environment } from 'src/environments/environment';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    StoreModule.forRoot({counter: counterReducer}),
+    StoreModule.forRoot(appReducer),
     StoreDevtoolsModule.instrument({ 
       logOnly:environment.production
     })
