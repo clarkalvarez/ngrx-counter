2d65a9c8fd0199956757c7bbdd372d4391d79f8c
clarkalvarez <clarknikka012@gmail.com>
2022-07-14 06:41:06 +0800

    10. Implement Routing for the state management Ngrx application - Angular Ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { CounterButtonsComponent } from './counter-buttons/counter-buttons.component';
-import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { CounterComponent } from './counter/counter.component'; 
 import { StoreModule } from '@ngrx/store'; 
-import { counterReducer } from './state/counter.reducer';
-import { CustomCounterInputComponent } from './custom-counter-input/custom-counter-input.component';
+import { counterReducer } from './counter/state/counter.reducer';
+import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
 import { FormsModule } from '@angular/forms';
-
+import { HomeComponent } from './home/home.component';
+import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
+import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component'; 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
     CounterButtonsComponent,
     CounterOutputComponent,
-    CustomCounterInputComponent
+    CustomCounterInputComponent,
+    HomeComponent,
+    HeaderComponent,
+    PostsListComponent
   ],
   imports: [
     BrowserModule,
