4a614e0529b2fd064cee4ae96a78b321f9e93142
clarkalvarez <clarknikka012@gmail.com>
2022-07-24 03:11:56 +0800

    39. Access ngrx state in CanActivate Route Guard for protecting routes in Angular Ngrx Application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'; 
 import { HomeComponent } from './home/home.component';  
+import { AuthGuard } from './services/auth.guard';
 
 
 const routes: Routes = [
@@ -14,7 +15,8 @@ const routes: Routes = [
   },
   {
     path: 'posts',
-    loadChildren: () => import('./posts/posts.module').then((m) => m.PostsModule)
+    loadChildren: () => import('./posts/posts.module').then((m) => m.PostsModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'auth',
