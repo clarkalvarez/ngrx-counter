19c53b8f7356f7f30bddc2f881166f82c4bdcb45
clarkalvarez <clarknikka012@gmail.com>
2022-07-24 10:36:54 +0800

    43. Use Router State Params in the selectors to get single post Data in Ngrx Angular Application.

--- src/app/posts/state/posts.selectors.ts
+++ src/app/posts/state/posts.selectors.ts
@@ -1,4 +1,6 @@
 import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { RouterStateUrl } from "src/app/router/custom-serializer";
+import { getCurrentRoute } from "src/app/router/router.selector";
 import {PostsState} from './posts.state'
 
 export const POST_STATE_NAME = 'posts';
@@ -9,9 +11,9 @@ export const getPosts = createSelector(getPostsState, state => {
     return state.posts
 }) 
 
-export const getPostById = createSelector(getPostsState, (state: any, props: any) => {
-    return state.posts.find((post: any) => post.id === props.id)
-}) 
+export const getPostById = createSelector(getPostsState, getCurrentRoute, (state: any, route: RouterStateUrl) => {
+    return state.posts ? state.posts.find((post:any) => post.id === route.params['id']) : null
+})  
 
 export const deletePost = createSelector(getPostsState, (state: any, props: any) => {
     return state.posts.find((post: any) => post.id === props.id)
